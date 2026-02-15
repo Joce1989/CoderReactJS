81e8db716050411c93e0ae81981599e83e563272
Jose Guzman <guzmanjosealfredo@gmail.com>
2026-02-15 13:11:00 -0300

    mejorando codigo

--- src/components/NavBarBS.jsx
+++ src/components/NavBarBS.jsx
@@ -14,17 +14,16 @@ function NavBarBS() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Nosotros</Nav.Link>
             <NavDropdown title="Productos" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Remeras</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Bombachas
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Ofertas
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
